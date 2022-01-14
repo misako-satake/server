d56e43804ef895e67324e53f2a9af24516f20651
misako-satake <misako.satake@rakus-partners.co.jp>
2022-01-14 09:00:29 +0900

    作成完了

--- src/main/java/com/example/demo/Test2Application.java
+++ src/main/java/com/example/demo/Test2Application.java
@@ -2,9 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+importorg.springframework.boot.builder.SpringApplicationBuilder;
+importorg.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Test2Application {
+public class Test2Application extends SpringBootServletInitializer{
+
+@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder 
+application) {
+        return application.sources(Test2Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test2Application.class, args);
